db051cdbc9402abe626351c49231ddda5a5e3906
Serhii Hovorov <seh@sk.ua>
2021-02-17 15:03:35 +0200

    add-prometheus

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from core import create_app
-# from app.core import create_app
-
+from prometheus_flask_exporter import PrometheusMetrics
 def myconnect():
     while True:
         try:
@@ -9,7 +8,9 @@ def myconnect():
             print("api isn't connected to database yet")
 
 
+
 app = myconnect()
+metrics = PrometheusMetrics(app)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=4000)
